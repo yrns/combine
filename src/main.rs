ca55c3fed75642efa9acdf67ea29650d80f3a906
Al McElrath <hello@yrns.org>
2020-12-24 11:22:21 -0700

    combine

--- src/main.rs
+++ src/main.rs
@@ -1,46 +1,128 @@
-use easy_parallel::Parallel;
-use futures::stream::StreamExt;
-use futures_signals::signal::{Mutable, Signal, SignalExt};
+use futures_signals::signal::Signal;
 use pin_project::pin_project;
 use std::fmt::Debug;
 use std::pin::Pin;
 use std::task::{Context, Poll};
 
-#[pin_project(project = CombineProj)]
-struct Combine<A, B, F>
-where
-    A: Signal,
-    B: Signal,
-{
-    #[pin]
-    a: A,
-    #[pin]
-    b: B,
-    last_a: Option<A::Item>,
-    last_b: Option<B::Item>,
-    f: F,
+trait PollAll<T> {
+    fn poll_all(self: Pin<&mut Self>, cx: &mut Context) -> Option<T>;
 }
 
-fn combine<A, B, C, F>(a: A, b: B, f: F) -> Combine<A, B, F>
+impl<A, B> PollAll<(Option<A::Item>, Option<B::Item>)> for Inputs2<A, B>
 where
     A: Signal,
     B: Signal,
-    F: Fn(A::Item, B::Item) -> C,
 {
-    Combine {
-        a,
-        b,
-        last_a: None,
-        last_b: None,
-        f,
+    fn poll_all(
+        self: Pin<&mut Self>,
+        cx: &mut Context,
+    ) -> Option<(Option<A::Item>, Option<B::Item>)> {
+        let mut done = 0;
+        let mut this = self.project();
+        let p = (
+            {
+                match this.0.as_mut().as_pin_mut().map(|s| s.poll_change(cx)) {
+                    None => {
+                        done += 1;
+                        None
+                    }
+                    Some(Poll::Pending) => None,
+                    Some(Poll::Ready(None)) => {
+                        this.0.set(None);
+                        done += 1;
+                        None
+                    }
+                    Some(Poll::Ready(a)) => a,
+                }
+            },
+            {
+                match this.1.as_mut().as_pin_mut().map(|s| s.poll_change(cx)) {
+                    None => {
+                        done += 1;
+                        None
+                    }
+                    Some(Poll::Pending) => None,
+                    Some(Poll::Ready(None)) => {
+                        this.1.set(None);
+                        done += 1;
+                        None
+                    }
+                    Some(Poll::Ready(a)) => a,
+                }
+            },
+        );
+
+        if done == 2 {
+            None
+        } else {
+            Some(p)
+        }
     }
 }
 
-impl<A, B, C, F> Signal for Combine<A, B, F>
+pub enum Input<T> {
+    Changed(T),
+    NotChanged(T),
+}
+
+impl<T> std::ops::Deref for Input<T> {
+    type Target = T;
+
+    fn deref(&self) -> &Self::Target {
+        match self {
+            Self::Changed(v) | Self::NotChanged(v) => v,
+        }
+    }
+}
+
+// This exists solely for projection since pin-project doesn't handle
+// tuple struct members.
+#[pin_project]
+#[derive(Debug)]
+struct Inputs2<A, B>(#[pin] Option<A>, #[pin] Option<B>);
+
+#[pin_project]
+#[derive(Debug)]
+pub struct Combine2<A, B, C, F>
 where
     A: Signal,
     B: Signal,
-    F: Fn(A::Item, B::Item) -> C,
+    F: Fn(Input<&A::Item>, Input<&B::Item>) -> C,
+{
+    #[pin]
+    inputs: Inputs2<A, B>,
+    last: (Option<A::Item>, Option<B::Item>),
+    f: F,
+}
+
+impl<A, B, C, F> Combine<F, Combine2<A, B, C, F>> for (A, B)
+where
+    A: Signal,
+    B: Signal,
+    F: Fn(Input<&A::Item>, Input<&B::Item>) -> C,
+{
+    fn combine(self, f: F) -> Combine2<A, B, C, F> {
+        Combine2 {
+            inputs: Inputs2(Some(self.0), Some(self.1)),
+            last: (None, None),
+            f,
+        }
+    }
+}
+
+pub trait Combine<F, T> {
+    fn combine(self, f: F) -> T;
+}
+
+pub fn combine<S, T: Combine<F, S>, F>(inputs: T, f: F) -> S {
+    inputs.combine(f)
+}
+
+impl<A, B, C, F> Signal for Combine2<A, B, C, F>
+where
+    A: Signal,
+    B: Signal,
+    F: Fn(Input<&A::Item>, Input<&B::Item>) -> C,
     A::Item: Debug,
     B::Item: Debug,
     C: Debug,
@@ -48,59 +130,78 @@ where
     type Item = C;
 
     fn poll_change(self: Pin<&mut Self>, cx: &mut Context) -> Poll<Option<Self::Item>> {
-        let CombineProj {
-            a,
-            b,
-            last_a,
-            last_b,
-            f,
-        } = self.project();
+        let mut this = self.project();
+        let mut last = &mut this.last;
 
-        match dbg!((
-            a.poll_change(cx),
-            b.poll_change(cx),
-            last_a.take(),
-            last_b.take()
-        )) {
-            (Poll::Ready(None), _, _, _) | (_, Poll::Ready(None), _, _) => Poll::Ready(None),
-            (Poll::Ready(Some(a)), Poll::Pending, _, Some(b))
-            | (Poll::Pending, Poll::Ready(Some(b)), Some(a), _)
-            | (Poll::Ready(Some(a)), Poll::Ready(Some(b)), _, _) => Poll::Ready(Some(f(a, b))),
-            (Poll::Ready(Some(a)), Poll::Pending, _, None) => {
-                *last_a = Some(a);
-                Poll::Pending
+        match this.inputs.poll_all(cx) {
+            Some(p) => {
+                let ready = (
+                    match p.0 {
+                        Some(a) => {
+                            last.0 = Some(a);
+                            last.0.as_ref().map(Input::Changed)
+                        }
+                        None => last.0.as_ref().map(Input::NotChanged),
+                    },
+                    match p.1 {
+                        Some(a) => {
+                            last.1 = Some(a);
+                            last.1.as_ref().map(Input::Changed)
+                        }
+                        None => last.1.as_ref().map(Input::NotChanged),
+                    },
+                );
+
+                if let (Some(a), Some(b)) = ready {
+                    Poll::Ready(Some((this.f)(a, b)))
+                } else {
+                    Poll::Pending
+                }
             }
-            (Poll::Pending, Poll::Ready(Some(b)), None, _) => {
-                *last_b = Some(b);
-                Poll::Pending
-            }
-            (Poll::Pending, Poll::Pending, a, b) => {
-                *last_a = a;
-                *last_b = b;
-                Poll::Pending
-            }
+            _ => Poll::Ready(None),
         }
     }
 }
 
-fn main() {
-    let m = Mutable::new(0usize);
-    let s1 = m.signal().map(|v| v * 2);
-    let s2 = m.signal().map(|v| v + 1);
-    let mut sum = combine(s1, s2, |a, b| a + b).to_stream();
+#[cfg(test)]
+mod tests {
+    use super::{Combine, Input};
+    use futures_lite::{future::block_on, StreamExt};
+    use futures_signals::signal::{Mutable, SignalExt};
 
-    Parallel::new()
-        .add(|| {
-            futures::executor::block_on(async {
-                assert_eq!(sum.next().await, Some(1usize));
-                assert_eq!(sum.next().await, Some(7usize));
-                //assert_eq!(sum.next().await, None);
-            });
-        })
-        .add(|| {
-            std::thread::sleep(std::time::Duration::from_millis(1000));
-            dbg!("set to 1!");
-            m.set(2);
-        })
-        .run();
+    #[test]
+    fn combine2() {
+        let m = Mutable::new(1usize);
+        let s1 = m.signal().map(|v| v * 40);
+        let s2 = m.signal().map(|v| v + 1);
+        let sum = (s1, s2).combine(|a, b| *a + *b).to_stream();
+
+        block_on(async {
+            assert_eq!(sum.take(1).collect::<Vec<_>>().await, vec![42]);
+        });
+    }
+
+    #[test]
+    fn changes() {
+        let m1 = Mutable::new(0usize);
+        let m2 = Mutable::new(0usize);
+        let mut changes = (m1.signal(), m2.signal())
+            .combine(|a, b| match (a, b) {
+                (Input::Changed(_), Input::Changed(_)) => 3,
+                (Input::NotChanged(_), Input::Changed(_)) => 2,
+                (Input::Changed(_), Input::NotChanged(_)) => 1,
+                _ => panic!(0),
+            })
+            .to_stream();
+
+        block_on(async {
+            assert_eq!(changes.next().await, Some(3));
+            m2.set(1);
+            assert_eq!(changes.next().await, Some(2));
+            m1.set(1);
+            assert_eq!(changes.next().await, Some(1));
+        });
+    }
 }
+
+fn main() {}
